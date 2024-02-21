8381606aadb372ceebf9e0d64ad3d932fb09f6e5
Dorin Laurentiu <dorin.vatan@plentysystems.com>
2024-02-21 11:30:53 +0200

    feat: AB#95351: [Telemetry] Add debug log

--- pkg/telemetry/telemetry.go
+++ pkg/telemetry/telemetry.go
@@ -59,6 +59,7 @@ type Tracer interface {
 type Logger interface {
 	Info(string, io.ReadCloser) error
 	Error(string, io.ReadCloser) error
+	Debug(string, io.ReadCloser) error
 }
 
 // Allocator ...
@@ -354,6 +355,18 @@ func (tc *TransactionContainer) Error(segmentID string, err *error) {
 		err := transaction.Error(segmentID, rc)
 		if err != nil {
 			log.Printf("%s%s Function: Error | Error: %v", TelemetryDriverError, driverName, err)
+		}
+	}
+}
+
+// Debug logs debug in the registered driver transactions
+// If segmentID is empty, the info will be logged directly on the transaction
+func (tc *TransactionContainer) Debug(segmentID string, msg *string) {
+	for driverName, transaction := range tc.transactions {
+		rc := io.NopCloser(strings.NewReader(*msg))
+		err := transaction.Debug(segmentID, rc)
+		if err != nil {
+			log.Printf("%s%s | Function: Debug | Error: %v", TelemetryDriverError, driverName, err)
 		}
 	}
 }
